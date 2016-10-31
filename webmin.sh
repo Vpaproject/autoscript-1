ea74c2ac54d041be0f625f19d7b6baedf1b11e3c
MuLuu09 <muluu09@gmail.com>
2016-11-01 04:45:12 +0800

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+apt-get update;
+apt-get -y upgrade;
+apt-get -y install wget curl;
 # webmin
 apt-get -y install webmin
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf

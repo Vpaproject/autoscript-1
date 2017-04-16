afae461c7ea5a9f248e314b5ebb0833cbf634f2b
MuLuu09 <muluu09@gmail.com>
2017-04-16 09:23:07 +0800

    Update debian32.sh

--- debian32.sh
+++ debian32.sh
@@ -64,7 +64,7 @@ wget -O /etc/openvpn/1194.conf "http://raw.github.com/MuLuu09/conf/master/1194-d
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "http://zakidotmy.5gbfree.com/debian32/iptables.up.rules"
+wget -O /etc/iptables.up.rules "http://raw.github.com/MuLuu09/conf/master/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
